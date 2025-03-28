d5cf65e97e18a8a36718f6f0b50c8d74c94369d3
itsAnian <anian.seidl@gmail.com>
2025-03-28 19:59:02 +0100

    Update flake & reformat

--- flake.nix
+++ flake.nix
@@ -1,53 +1,49 @@
 {
-  description = "A development environment for Node.js";
+    description = "A development environment for Node.js";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        # Development shell
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs
-            pkgs.nodePackages.npm
-            pkgs.ejs
-            pkgs.docker
-            # Optionally, add yarn if you prefer it over npm
-            # pkgs.yarn
-          ];
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+                let
+                pkgs = nixpkgs.legacyPackages.${system};
+                in
+                {
+                devShells.default = pkgs.mkShell {
+                buildInputs = [
+                pkgs.nodejs
+                pkgs.nodePackages.npm
+                pkgs.ejs
+                ];
 
-          shellHook = ''
-            echo "Node.js environment ready!"
-            echo "Node.js version: $(node --version)"
-            echo "npm version: $(npm --version)"
-            npm install express
-          '';
-        };
+                shellHook = ''
+                echo "Node.js environment ready!"
+                echo "Node.js version: $(node --version)"
+                echo "npm version: $(npm --version)"
+                npm install express
+                npm install sqlite3
+                '';
+                };
 
-        # Default package (buildable with `nix build`)
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "my-nodejs-project";
-          src = ./.;
-          buildInputs = [ pkgs.nodejs pkgs.ejs ];
-          buildPhase = ''
-            npm install express
-          '';
-          installPhase = ''
-            npm start
-          '';
-        };
+                packages.default = pkgs.stdenv.mkDerivation {
+                    name = "my-nodejs-project";
+                    src = ./.;
+                    buildInputs = [ pkgs.nodejs pkgs.ejs ];
+                    buildPhase = ''
+                        npm install express
+                        npm install sqlite3
+                        '';
+                    installPhase = ''
+                        npm start
+                        '';
+                };
 
-        # Default app (runnable with `nix run`)
-        apps.default = {
-          type = "app";
-          program = "npm start";
-        };
-      });
+                apps.default = {
+                    type = "app";
+                    program = "npm start";
+                };
+                });
 }
